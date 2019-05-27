694e00aa1035cf5cb6dc781f6b9af4e0d2d4c80b
mceylan35 <mehmetceylan2696@gmail.com>
2019-05-28 00:06:48 +0300

    Initial commit

---
+++ src/main/java/com/springboot/students/University/UniversityApplication.java
@@ -0,0 +1,13 @@
+package com.springboot.students.University;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UniversityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UniversityApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/springboot/students/University/UniversityApplicationTests.java
@@ -0,0 +1,16 @@
+package com.springboot.students.University;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class UniversityApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
